83d7a5b2e5ba003696ae72d5f8d0ba24e8992091
Nihal Machhi <nihalmachhi11@gmail.com>
2025-11-22 19:33:22 +0530

    input output - 2

--- inputoutput.py
+++ inputoutput.py
@@ -1,2 +1,40 @@
-name = input("what is your majors")
-print(  "ofc", name, ":)")
+# name = input("what is your majors")
+# print(  "ofc", name, ":)")
+
+
+# x, y = input("Enter two values: ").split()
+# print("Number of boys: ", x)
+# print("Number of girls: ", y)
+
+ 
+# x, y, z = input("Enter three values: ").split()
+# print("Total number of students: ", x)
+# print("Number of boys is : ", y)
+# print("Number of girls is : ", z)
+# print(type(z))
+
+
+# n = int(input('enter your fav number'))
+# print(n)
+
+# print(type(n))
+
+# n = float(input('enter your fav number'))
+# print(n)
+
+# print(type(n))
+
+a = "Hello World"
+b = 10
+c = 11.22
+d = ("Geeks", "for", "Geeks")
+e = ["Geeks", "for", "Geeks"]
+f = {"Geeks": 1, "for":2, "Geeks":3}
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
