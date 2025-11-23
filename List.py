b1df1cf2eceaf950ac8e01f6c53bc82f2d29a3b9
Nihal Machhi <nihalmachhi11@gmail.com>
2025-11-23 09:10:23 +0530

    List-Python

---
+++ List.py
@@ -0,0 +1,62 @@
+number = [1,2,3,4,5]
+
+# print(number)
+
+
+# number.append(6)
+
+# print(number)
+
+
+# number.insert(1,3)
+# print(number)
+
+
+# print(number[0])
+# print(number[1])
+
+
+# for i in number:
+#     print(number)
+
+
+# print(len(number))
+
+
+# number[2] = 300
+# print(number)
+
+# number[1:3] = [111, 222]
+# print(number)
+
+
+# number.remove(5)
+# number.pop(2)
+
+# del number[2]
+
+# number.clear()
+# print(number)  
+
+# print(number)
+
+
+
+fruits = ["apple", "banana", "mango"]
+print("Initial:", fruits)
+
+
+print("First fruit:", fruits[0])
+for f in fruits:
+    print("Fruit:", f)
+
+
+fruits[1] = "orange"
+print("After update:", fruits)
+
+
+fruits.remove("mango")
+print("After delete:", fruits)
+
+fruits.append("grapes")
+print("Final list:", fruits)
