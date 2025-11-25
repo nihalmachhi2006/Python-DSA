276ece0ef58d5317b229dadf99bbb7caf45811e0
Nihal Machhi <nihalmachhi11@gmail.com>
2025-11-25 11:04:47 +0530

    BubbleSort

---
+++ DSA/Sorting/bubblesort.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+arr = [25,36,96,45,22,11,96]
+
+sortarr = bubble_sort(arr)
+
+print(sortarr)
---
+++ oops/class_objects.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("MNihal")
+s2 = Student("Kihal")
+print(s1.name)
+print(s2.name)
